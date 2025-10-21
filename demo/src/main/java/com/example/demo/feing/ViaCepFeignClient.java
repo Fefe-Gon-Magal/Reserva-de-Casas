59b7039a7bdf27966a5ce1d44fd9c1939aed435e
Fefe-Gon-Magal <fernadagoncalvesmagalhaes@gmail.com>
2025-10-21 16:17:45 -0300

    Change.ViaCepServie

---
+++ demo/src/main/java/com/example/demo/feing/ViaCepFeignClient.java
@@ -0,0 +1,17 @@
+package com.example.demo.feing;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.dto.ViaCepResponseDTO;
+
+@FeignClient (name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepFeignClient 
+{
+
+    @GetMapping("/{cep}/json/")
+    ViaCepResponseDTO buscarCep(@PathVariable("cep") String cep);
+
+    
+}
--- demo/src/main/java/com/example/demo/service/ViaCepService.java
+++ demo/src/main/java/com/example/demo/service/ViaCepService.java
@@ -4,20 +4,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.dto.ViaCepResponseDTO;
+import com.example.demo.feing.ViaCepFeignClient;
 
 // Criação de um Service para uma API dia 26.09.25
 @Service
 public class ViaCepService 
 {
-    private static final String VIA_CEP_URL = "https://viacep.com.br/ws/{cep}/json/";
-    
-    public ViaCepResponseDTO buscarCep(String cep)
+  
+    private final ViaCepFeignClient viaCepFeignClient; 
+
+    public ViaCepService(ViaCepFeignClient viaCepFeignClient)
     {
-        RestTemplate restTemplate = new RestTemplate();
-        
-        // Faz uma chamada GET para a API ViaCEP e converte a resposta em ViaCepResponseDTO
-        return restTemplate.getForObject(VIA_CEP_URL, ViaCepResponseDTO.class, cep);
+        this.viaCepFeignClient = viaCepFeignClient;
     }
 
-
+    public ViaCepResponseDTO buscarCep(String cep)
+    {
+        return viaCepFeignClient.buscarCep (cep);
+    }
 }
